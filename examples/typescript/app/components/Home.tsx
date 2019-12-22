f9a30500588ece2842b2e0bca5163399c566d921
David Acheson <dacheson@microsoft.com>
2019-12-22 08:26:57 -0800

    First Update

--- examples/typescript/app/components/Home.tsx
+++ examples/typescript/app/components/Home.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Component } from 'react';
-import { Link } from 'react-router-dom';
-const routes = require('../constants/routes.json');
+// import { Link } from 'react-router-dom';
+// const routes = require('../constants/routes.json');
 const styles = require('./Home.css');
 
 type Props = {};
@@ -12,8 +12,13 @@ export default class Home extends Component<Props> {
   render() {
     return (
       <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
+        <h3>Pick a position</h3>
+        <select>
+          <option value="punnani">1</option>
+          <option value="vagisil">2</option>
+          <option value="sore bum illness">3</option>
+          <option value="poof poof pop  ">4</option>
+        </select>
       </div>
     );
   }
