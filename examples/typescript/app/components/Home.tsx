90e80014b9b2b808d6d3a87a226800d42ec3a11d
David Acheson <dacheson@microsoft.com>
2019-12-22 11:41:38 -0800

    added basic ui

--- examples/typescript/app/components/Home.tsx
+++ examples/typescript/app/components/Home.tsx
@@ -12,6 +12,50 @@ export default class Home extends Component<Props> {
   render() {
     return (
       <div className={styles.container} data-tid="container">
+        { this.renderScheduleList() }
+        { this.renderDropdownMenu() }
+        { this.renderTimeSelection() }
+        { this.renderSubmitToListButton() }
+      </div>
+    );
+  }
+
+  renderSubmitToListButton() {
+    return (
+      <div>
+        <button type="submit" onClick={this.submitToListClicked}>Submit to List</button>
+      </div>
+    );
+  }
+
+  renderScheduleList() {
+    let x = ["Operation 1", "Operation 2", "Operation 3"];
+    return (
+      <ul>
+        { x.map((item,i) => <li key={i}>{item}</li>) }
+      </ul>
+    );
+  }
+
+  renderSublist() {
+    let x = ["Operation 1", "Operation 2", "Operation 3"];
+    return (
+        x.map((item,i) => <li key={i}>Test</li>)
+    );
+  }
+
+  renderTimeSelection() {
+    return (
+      <div>
+        <h3>Pick a time</h3>
+        <input type="text"/>
+      </div>
+    );
+  }
+
+  renderDropdownMenu() {
+    return (
+      <div>
         <h3>Pick a position</h3>
         <select>
           <option value="punnani">1</option>
@@ -21,5 +65,9 @@ export default class Home extends Component<Props> {
         </select>
       </div>
     );
+  }
+
+  submitToListClicked() {
+    alert("Hey ballbag");
   }
 }
